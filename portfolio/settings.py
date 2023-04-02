9d7dc320d85547c9a3ba61e3ae663aa8d6fa5db0
Kavin1421 <kkavinkumar24@gmail.com>
2023-04-02 13:23:08 +0530

    SecondPortfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,13 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9o^!5shqqavj@#@&36+y+nl3csjw2%hr3(wrzh@be05fd5ita)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'jameswaweru.herokuapp.com',
-    'jameswaweruweb.herokuapp.com',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ["*"]
+    # 'jameswaweru.herokuapp.com',
+    # 'jameswaweruweb.herokuapp.com',
+    # '127.0.0.1'
 
 # Application definition
 
