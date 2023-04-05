7302ced3dac541e07abffc0aa4b516eea0d1b4c3
Kavin1421 <kkavinkumar24@gmail.com>
2023-04-05 23:10:49 +0530

    FifthPortfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATIC_URL = '/static/'
 
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 MEDIA_URL = '/images/'
