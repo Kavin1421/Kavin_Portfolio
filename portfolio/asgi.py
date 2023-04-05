1b6bebb4636ae4a2254996c552624d97cb56177f
Kavin1421 <kkavinkumar24@gmail.com>
2023-04-05 23:00:41 +0530

    FourthPortfolio

--- portfolio/asgi.py
+++ portfolio/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+# import django.core.asgi
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
