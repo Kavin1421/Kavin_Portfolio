8d5e28176e5e5c93728629088b9b8206fb5c1ecc
Kavin1421 <kkavinkumar24@gmail.com>
2023-04-05 22:13:41 +0530

    ThirdPortfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
